96a301e354c008c7f17a4a4430230fa35b12c88f
zanseven007 <445897574@qq.com>
2016-09-10 16:14:16 +0800

    新建模板项目

---
+++ app/index.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var chalk = require('chalk');    //不同颜色的info
+var util = require('util');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');    //yeoman弹出框
+var path = require('path');
+var Reactpackage = yeoman.Base.extend({
+    info: function() {
+        this.log(chalk.green(
+            'I am going to build your H5 templates!'
+        ));
+    },
+    generateBasic: function() {  //按照自己的templates目录自定义
+        this.directory('src', 'src');    //拷贝目录
+        this.directory('dist', 'dist');
+        this.copy('package.json', 'package.json');   //拷贝文件
+        this.copy('index.html', 'index.html');
+        this.copy('README.md', 'README.md');
+        this.copy('webpack.config.js', 'webpack.config.js');
+    },
+    generateClient: function() {
+        this.sourceRoot(path.join(__dirname, 'templates'));
+        this.destinationPath('./');
+    },
+    install: function() {      //安装依赖
+        this.installDependencies({
+            skipInstall: this.options['skip-install']
+        });
+    },
+    end: function() {
+        this.log(yosay(
+            'Your H5 templates has been created successfully!'
+        ));
+    }
+});
+module.exports = Reactpackage;
---
+++ app/templates/webpack.config.js
@@ -0,0 +1,59 @@
+var webpack = require('webpack');
+var path = require('path');
+var autoprefixer = require('autoprefixer');
+
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'src');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
+
+module.exports = {
+    devtool: 'source-map',
+
+    entry: './src/**/*.js',
+
+    output: {
+        path: BUILD_PATH,
+        filename: 'main.min.js'
+    },
+
+    devServer: {
+        publicPath: "/dist/"
+    },
+
+    module: {
+        // noParse: /node_modules\/hls.js\/dist\/hls.js/,
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                include: APP_PATH,
+                query: {
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.(css|scss)$/,
+                loaders: ['style', 'css', 'postcss', 'sass'],
+                include: APP_PATH
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=8192'
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+    ],
+
+    postcss: [
+        autoprefixer({
+            browsers: ['last 15 versions', '> 1%', 'ie 8', 'ie 7']
+        })
+    ]
+};
